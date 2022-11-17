58cf7c981644e6ed6793cc6ee3135dbea54a412d
David Apollo <davidapollo11@hotmail.com>
2022-11-17 12:40:22 -0300

    aplication styles, global and theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+    PINK_100: "#282124",
+
+    GRAY_100: "#948F99",
+
+    RED: "#FF002E"
+  }
+}
