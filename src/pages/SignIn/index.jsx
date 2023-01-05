dddfcdc3e574ee0e77493470758f42edaf4dc87c
David Apollo <davidapollo11@hotmail.com>
2023-01-05 20:46:19 -0300

    add hook auth

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
 
 import { FiMail, FiLock } from 'react-icons/fi'
 
@@ -8,6 +11,15 @@ import { Input } from '../../components/Input'
 import { Container, Form, Background } from './styles'
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
@@ -17,11 +29,11 @@ export function SignIn() {
         <h2>Faça seu login</h2>
 
         <div>
-          <Input className='input' type='email' icon={FiMail} placeholder='E-mail' />
-          <Input className='input' type='password' icon={FiLock} placeholder='E-mail' />
+          <Input className='input' type='email' icon={FiMail} placeholder='E-mail' onChange={e => setEmail(e.target.value)} />
+          <Input className='input' type='password' icon={FiLock} placeholder='Senha' onChange={e => setPassword(e.target.value)} />
         </div>
 
-        <Button title='Entrar' />
+        <Button title='Entrar' onClick={handleSignIn} />
 
         <Link to='/register'>Criar conta</Link>
       </Form>
